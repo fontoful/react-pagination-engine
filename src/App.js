a7426acbad5ba06076b5923fc81cd597772a45c9
fontoful <40501207+fontoful@users.noreply.github.com>
2019-08-18 19:15:50 -0600

    components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [current, setCurrent] = useState(1);
+    const [postsPerPage, setpostsPerPage] = useState(10);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            const res = await axios.get(
+                "http://jsonplaceholder.typicode.com/posts"
+            );
+            setPosts(res.data);
+            setLoading(false);
+            console.log(res.data);
+        };
+
+        fetchPosts();
+    }, []);
+
+    // Get current posts
+    const indexOfLastPost = current * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = pageNumber => setCurrent(pageNumber);
+
+    return (
+        <div className="container mt-5">
+            <p className="text-primary mb-3">My Blog</p>
+            <Posts posts={currentPosts} loading={loading} />
+            <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={posts.length}
+                paginate={paginate}
+            />
+        </div>
+    );
+};
 
 export default App;
